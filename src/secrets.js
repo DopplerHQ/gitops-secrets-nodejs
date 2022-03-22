24efacb356705bab5c52af9713bf07d310a3d041
Ryan Blunden <ryan.blunden@doppler.com>
2022-03-22 21:40:12 +1000

    Simplified encryption and decryption methods (#16)
    - Simplified encryption and decryption
    - Key length hard-coded as it does not need to be configurable
    - Changed chunk separator to `:`
    - Replaced repeated strings with constants
    -  Add test to ensure decryption backwards compatibility

--- src/secrets.js
+++ src/secrets.js
@@ -1,14 +1,14 @@
 const crypto = require("crypto");
 
 const PBKDF2_ROUNDS = process.env.GITOPS_SECRETS_PBKDF2_ROUNDS || 1000000;
-const PBKDF2_KEYLEN = process.env.GITOPS_SECRETS_PBKDF2_KEYLEN || 32;
+const PBKDF2_KEYLEN = 32;
 const PBKDF2_DIGEST = "sha256";
 const ALGORITHM = "aes-256-gcm";
 const AES_AUTH_TAG_BYTES = 16;
 const AES_IV_BYTES = 12;
 const AES_SALT_BYTES = 8;
 const ENCODING = "base64";
-const ENCODING_PREFIX = "base64:";
+const TEXT_ENCODING = "utf8";
 
 function masterKey() {
   if (!process.env.GITOPS_SECRETS_MASTER_KEY || process.env.GITOPS_SECRETS_MASTER_KEY.length < 16) {
@@ -26,18 +26,13 @@ function masterKey() {
 function encrypt(secrets) {
   const salt = crypto.randomBytes(AES_SALT_BYTES);
   const iv = crypto.randomBytes(AES_IV_BYTES);
-
-  // construct key
   const key = crypto.pbkdf2Sync(masterKey(), salt, PBKDF2_ROUNDS, PBKDF2_KEYLEN, PBKDF2_DIGEST);
-
   const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
-  const cipherText = Buffer.concat([cipher.update(JSON.stringify(secrets), "utf8"), cipher.final()]);
+  const cipherText = Buffer.concat([cipher.update(JSON.stringify(secrets), TEXT_ENCODING), cipher.final()]);
   const authTag = cipher.getAuthTag();
   const combinedData = Buffer.concat([cipherText, authTag]);
 
-  return `${ENCODING_PREFIX}${PBKDF2_ROUNDS}-${PBKDF2_KEYLEN}-${salt.toString("base64")}-${iv.toString(
-    "base64"
-  )}-${combinedData.toString("base64")}`;
+  return `${ENCODING}:${PBKDF2_ROUNDS}:${salt.toString(ENCODING)}:${iv.toString(ENCODING)}:${combinedData.toString(ENCODING)}`;
 }
 
 /**
@@ -46,28 +41,27 @@ function encrypt(secrets) {
  * @returns {string}
  */
 function decrypt(secrets) {
-  secrets = secrets.substring(ENCODING_PREFIX.length);
+  secrets = secrets.substring(`${ENCODING}:`.length);
 
   // Decode file contents
-  const parts = secrets.split("-");
-  if (parts.length !== 5) {
-    throw new Error(`Encrypted payload invalid. Expected 5 sections but only got ${parts.length}`);
+  const parts = secrets.split(":");
+  if (parts.length !== 4) {
+    throw new Error(`Encrypted payload invalid. Expected 4 sections but only got ${parts.length}`);
   }
 
-  const rounds = parseInt(Buffer.from(parts[0], "utf8"), 10);
-  const keyLength = parseInt(Buffer.from(parts[1], "utf8"), 10);
-  const salt = Buffer.from(parts[2], ENCODING);
-  const iv = Buffer.from(parts[3], ENCODING);
-  const data = Buffer.from(parts[4], ENCODING);
+  const rounds = parseInt(Buffer.from(parts[0], TEXT_ENCODING), 10);
+  const salt = Buffer.from(parts[1], ENCODING);
+  const iv = Buffer.from(parts[2], ENCODING);
+  const data = Buffer.from(parts[3], ENCODING);
   const cipherText = data.slice(0, data.length - AES_AUTH_TAG_BYTES);
   const authTag = data.slice(data.length - AES_AUTH_TAG_BYTES);
 
   // construct key
-  const key = crypto.pbkdf2Sync(masterKey(), salt, rounds, keyLength, PBKDF2_DIGEST);
+  const key = crypto.pbkdf2Sync(masterKey(), salt, rounds, PBKDF2_KEYLEN, PBKDF2_DIGEST);
 
   // decrypt cipher text
   const decipher = crypto.createDecipheriv(ALGORITHM, key, iv).setAuthTag(authTag);
-  const decrypted = decipher.update(cipherText, "binary", "utf8") + decipher.final("utf8");
+  const decrypted = decipher.update(cipherText, "binary", TEXT_ENCODING) + decipher.final("utf8");
   return JSON.parse(decrypted);
 }
 
